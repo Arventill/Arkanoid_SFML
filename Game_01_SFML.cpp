6a4075641fad2932a3fa39320a8dd2360d5a2c82
Filip Nowicki <39906425+arventill@users.noreply.github.com>
2019-01-18 17:56:30 +0100

    Add files via upload
    feature 1 - Stworzenie pierwszej wersji "GAME OVER"

--- Game_01_SFML.cpp
+++ Game_01_SFML.cpp
@@ -1,4 +1,4 @@
-// Game_01_SFML.cpp : This is Arkanoid
+// Game_01_SFML.cpp : This is Arkanoid SFML Game
 
 #include "Ball.h"
 #include "Pad.h"
@@ -6,48 +6,64 @@
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
-#include <SFML/System.hpp>
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <cwchar>
 
 unsigned int tillTheEnd = 36;
+const short windX = 800;	 //window
+const short windY = 600;
+const short ballPosX = 400;	 //ball
+const short ballPosY = 300;
+const short padPosX = 400;	 //pad
+const short padPosY = 550;
+const short columns = 9;	 //bricks
+const short rows = 4;
+const short brickW = 60;
+const short brickH = 20;
+
+#pragma region HelperMethods
 template <class T1, class T2> bool isIntersecting(T1& a, T2& b) //uniwersalna funkcja zwracaj¹ca wart. bool dla 2 obiektów dowolnego typu
 {
 	return a.right() >= b.left() && a.left() <= b.right()
 		&& a.top() <= b.bot() && a.bot() >= b.top();
 }
+
 bool ColisionTest(Brick& bBrick, Ball& bBall)
 {
-	if (!isIntersecting(bBrick, bBall)) return false;
-
-	bBrick.destroy(); //else
-	tillTheEnd--;
-
-	float overlapLeft{ bBall.right() - bBrick.left() };
-	float overlapRight{ bBrick.left() - bBall.right() };
-	float overlapTop{ bBall.bot() - bBrick.top() };
-	float overlapBot{ bBrick.bot() - bBall.top() };
-
-	bool ballFromLeft(abs(overlapLeft) < abs(overlapRight));
-	bool ballFromTop(abs(overlapTop) < abs(overlapBot));
-
-	float minOverlapX{ ballFromLeft ? overlapLeft : overlapRight };
-	float minOverlapY{ ballFromTop ? overlapTop : overlapBot };
-
-	if (abs(minOverlapX) < abs(minOverlapY))
-	{
-		ballFromLeft ? bBall.moveLeft() : bBall.moveRight();
-	}
+	if (!isIntersecting(bBrick, bBall))
+		return false;
 	else
 	{
-		ballFromTop ? bBall.moveUp() : bBall.moveDown();
+		bBrick.destroy();
+		tillTheEnd--;
+
+		float overlapLeft{ bBall.right() - bBrick.left() };
+		float overlapRight{ bBrick.left() - bBall.right() };
+		float overlapTop{ bBall.bot() - bBrick.top() };
+		float overlapBot{ bBrick.bot() - bBall.top() };
+
+		bool ballFromLeft(abs(overlapLeft) < abs(overlapRight));
+		bool ballFromTop(abs(overlapTop) < abs(overlapBot));
+
+		float minOverlapX{ ballFromLeft ? overlapLeft : overlapRight };
+		float minOverlapY{ ballFromTop ? overlapTop : overlapBot };
+
+		if (abs(minOverlapX) < abs(minOverlapY))
+			ballFromLeft ? bBall.moveLeft() : bBall.moveRight();
+		else
+			ballFromTop ? bBall.moveUp() : bBall.moveDown();
 	}
 }
+#pragma endregion
 
 int main()
 {
 #pragma region onCreate
-	Ball ball(400, 300); //ball position
-	Pad pad(400, 550); //pad position
-	unsigned brickX{ 9 }, brickY{ 4 }, brickWidth{ 60 }, brickHeight{ 20 }; // blocks in row, rows, width, height
+	Ball ball(ballPosX, ballPosY);
+	Pad pad(padPosX, padPosY);
+	unsigned brickX{ columns }, brickY{ rows }, brickWidth{ brickW }, brickHeight{ brickH };
 
 	std::vector<Brick> bricks;
 	for (int i = 0; i < brickY; i++)
@@ -59,57 +75,58 @@ int main()
 		}
 	}
 
-	sf::RenderWindow window{ {800,600}, "Arkanoid Game" };
+	sf::RenderWindow window{ {windX,windY}, "Arkanoid Game" };
 	window.setFramerateLimit(60); //60ps
 #pragma endregion
 
+#pragma region MainLoop
 	while (window.isOpen())
 	{
-#pragma region WindowEvent
+		window.clear(sf::Color::Black);
+		for (auto& brick : bricks) //  <-- foreach loop
+		{
+			if (ColisionTest(brick, ball))
+				break;
+		}
+		auto iterator = std::remove_if(begin(bricks), end(bricks), [](Brick& brick)
+			{ return brick.isDestroyed(); });
+		bricks.erase(iterator, end(bricks));
+		for (auto& brick : bricks) //  <-- foreach loop
+		{ 
+			window.draw(brick); 
+		}
+		
 		sf::Event windowEvent;
 		while (window.pollEvent(windowEvent))
 		{
 			if (windowEvent.type == sf::Event::Closed) //closing my game by button
 				window.close();
 		}
-		window.clear(sf::Color::Black);
-#pragma endregion
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+			pad.right();
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+			pad.left();
 
-#pragma region forEachLoops
-		for (auto& brick : bricks) //  <-- foreach loop
-			if (ColisionTest(brick,ball))
-				break;
-
-		auto iterator = std::remove_if(begin(bricks), end(bricks), [](Brick& brick) {return brick.isDestroyed(); });
-		bricks.erase(iterator, end(bricks));
-
-		for (auto& brick : bricks) //  <-- foreach
-			window.draw(brick);
-#pragma endregion
-
-#pragma region drawDisplay
+		
 		ball.update(pad.currentPos(), pad.param());
 		window.draw(ball);
 		window.draw(pad);
 		window.display();
-#pragma endregion
-
-#pragma region padMoving
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-		{
-			pad.right();
-		}
-		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-		{
-			pad.left();
-		}
-#pragma endregion
-
+		
 		if (tillTheEnd == 0)
 		{
+			std::string gameOver = "GAME OVER";
+			_CONSOLE_FONT_INFOEX textProps;
+			textProps.dwFontSize.Y = 55;
 			ball.~Ball();
 			pad.~Pad();
+			window.clear(sf::Color::Black);
+			SetCursorPos( ((window.getSize().x / 2) - (gameOver.length() / 2)), window.getSize().y );
+			SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE),NULL,&textProps);
+			std::cout << gameOver;
+			Sleep(500);
 			window.close();
 		}
 	}
+#pragma endregion
 }
